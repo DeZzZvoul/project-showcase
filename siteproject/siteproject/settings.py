97812a4a6e477d80956e9221296f6d8c4c890645
Semyon <s.serbulov@g.nsu.ru>
2021-08-16 15:36:48 +0700

    projects complete

--- siteproject/siteproject/settings.py
+++ siteproject/siteproject/settings.py
@@ -127,7 +127,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT= os.path.join(BASE_DIR, 'static')
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
